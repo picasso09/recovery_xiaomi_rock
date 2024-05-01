3768ec940dd2cf855359afb756a600b74633436c
MisterZtr <andey2004586@gmail.com>
2024-05-01 16:31:45 +0300

    modules: Add apatch modules mount + new logic

--- recovery/root/system/bin/kernelsu.sh
+++
@@ -1,6 +0,0 @@
-#!/sbin/sh
-if [ -f "/data/adb/ksu/modules.img" ]; then
-  e2fsck -fy /data/adb/ksu/modules.img;
-  resize2fs -M /data/adb/ksu/modules.img;
-  mount -t auto -o loop /data/adb/ksu/modules.img /data/adb/modules;
-fi;
